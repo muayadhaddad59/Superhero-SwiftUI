bc6e94a587973c11a9e0d9076f564bf2fc68ba0b
Muayad Haddad <muayadhaddad@yahoo.com>
2022-08-04 17:38:25 +0300

    Initial Commit

---
+++ Superhero/SuperheroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperheroApp.swift
+//  Superhero
+//
+//  Created by Muayad Haddad on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperheroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
